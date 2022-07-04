ea9e5d6c1481ffbfada0d592d0186e902e315c8c
Baniomash <jonatas.e@escolar.ifrn.edu.br>
2022-07-04 07:55:44 -0300

    Logo on Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
-import { Button, Text } from "react-native";
+import { Button, Text, Image } from "react-native";
 import { Container, Content } from "./styles";
 
 interface HeaderProps {
   onOpenNewTransactionModal: () => void;
 }
-
 export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
-        {/* <Image source={require("./../../../assets/Logo")} /> */}
+        <Image source={require("./../../../assets/Logo.jpg")} />
         <Text>XMoney</Text>
         <Button
           title="Nova Transação"
