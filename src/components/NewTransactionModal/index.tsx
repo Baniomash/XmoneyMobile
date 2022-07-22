b0cf7f6e6e5b97ff82fe4b011a899db1e949ede1
Aluno <aluno@debian.ifrn.local>
2022-07-22 10:39:46 -0300

    Atualizando hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import { Modal, Text, Image } from "react-native";
+import React, { FormEvent, useState } from "react";
+import { Modal, Text } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
+import { useTransactions } from "../../hooks/useTransactions";
 
 import {
   BtnModal,
@@ -23,10 +24,32 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const { createTrasaction } = useTransactions();
+
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("Gasto Padrão");
-  const [type, setType] = useState("deposit");
+  const [type, setType] = useState("Depósito");
+  const [bank, setBank] = useState("Padrão");
+
+  async function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    await createTrasaction({
+      title,
+      amount,
+      category,
+      type,
+      bank,
+    });
+
+    setTitle("");
+    setAmount("0");
+    setCategory("");
+    setType("Depósito");
+    setBank("");
+    onRequestClose();
+  }
 
   return (
     <CenteredView>
@@ -49,7 +72,7 @@ export function NewTransactionModal({
             <TransactioTypeContainer>
               <RadioBox
                 onPress={() => {
-                  setType("deposit");
+                  setType("Depósito");
                 }}
               >
                 <Icon source={require("./../../../assets/Entradas.png")} />
@@ -57,7 +80,7 @@ export function NewTransactionModal({
               </RadioBox>
               <RadioBox
                 onPress={() => {
-                  setType("withdraw");
+                  setType("Retirada");
                 }}
               >
                 <Icon source={require("./../../../assets/Saidas.png")} />
@@ -68,30 +91,32 @@ export function NewTransactionModal({
               placeholder={{ label: "Selecione a categoria" }}
               style={{ inputAndroid: { backgroundColor: "#e7e9ee" } }}
               onValueChange={(value) => setCategory(value)}
+              value={category}
               items={[
-                { label: "TypeStript", value: "TypeStript" },
-                { label: "Python", value: "Python" },
-                { label: "Java", value: "Java" },
-                { label: "C++", value: "C++" },
-                { label: "C", value: "C" },
+                { label: "Moradia", value: "Moradia" },
+                { label: "Trabalho", value: "Trabalho" },
+                { label: "Diversão", value: "Diversão" },
+                { label: "Supermecado", value: "Supermecado" },
+                { label: "Saúde", value: "Saúde" },
               ]}
             />
             <RNPickerSelect
               placeholder={{ label: "Selecione banco utilizado" }}
               style={{
                 inputAndroid: {
-                  backgroundColor: "#ffffff",
+                  backgroundColor: "#e7e9ee",
                   marginTop: 8,
                   marginBottom: 8,
                 },
               }}
-              onValueChange={(value) => setCategory(value)}
+              onValueChange={(value) => setBank(value)}
+              value={bank}
               items={[
-                { label: "TypeStript", value: "TypeStript" },
-                { label: "Python", value: "Python" },
-                { label: "Java", value: "Java" },
-                { label: "C++", value: "C++" },
-                { label: "C", value: "C" },
+                { label: "À vista", value: "À vista" },
+                { label: "Itaú", value: "Itaú" },
+                { label: "NUBank", value: "NUBank" },
+                { label: "Santander", value: "Santander" },
+                { label: "Caixa", value: "Caixa" },
               ]}
             />
             <BtnModal onPress={onRequestClose}>
