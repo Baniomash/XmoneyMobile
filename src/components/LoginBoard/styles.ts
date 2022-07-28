b29cb303a8cd0075e981aeeb2e4930c72d7cdeb5
Baniomash <jonatas.e@escolar.ifrn.edu.br>
2022-07-27 21:35:11 -0300

    feat: criando a tela de login

---
+++ src/components/LoginBoard/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  background-color: #fff;
+  flex: 1;
+  justify-content: flex-end;
+  background-color: #f9f9f9;
+`;
--- src/screens/LoginPage.tsx
+++ src/screens/LoginPage.tsx
@@ -1,12 +1,15 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React from "react";
 import { Button, View } from "react-native";
 import { RootStackParams } from "../../App";
+import { LoginBoard } from "../components/LoginBoard";
 
 type Props = NativeStackScreenProps<RootStackParams>;
 
 export function LoginPage({ navigation }: Props) {
   return (
     <View style={{ alignItems: "center", margin: 50 }}>
+      <LoginBoard />
       <Button
         title="Login"
         onPress={() => {
