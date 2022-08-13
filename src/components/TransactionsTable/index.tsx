2ca9bc78cbe3d26d3465385d28536f7668795ce6
Hari Junior <antonio.hari@hotmail.com>
2022-08-13 20:37:30 -0300

    feat: add 2 another filters

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -25,16 +25,16 @@ export function TransactionsTable() {
   const [tipo, setTipo] = useState(undefined);
 
   const transactions = [
-    { id: 1, title: "Conta de Luz", type: true, amount: 100, bank: "Itau", category: "Moradia", createdAt: "2020-01-01" },
-    { id: 2, title: "Conta de Luz", type: false, amount: 100, bank: "Itau", category: "Trabalho", createdAt: "2020-01-01" },
-    { id: 3, title: "Conta de Luz", type: true, amount: 100, bank: "Itau", category: "Diversão", createdAt: "2020-01-01" },
-    { id: 4, title: "Conta de Luz", type: false, amount: 100, bank: "Itau", category: "Supermecado", createdAt: "2020-01-01" },
-    { id: 5, title: "Conta de Luz", type: true, amount: 100, bank: "Itau", category: "Saúde", createdAt: "2020-01-01" },
+    { id: 1, title: "Conta de Luz", type: true, amount: 100, bank: "Itaú", category: "Moradia", createdAt: "2020-01-01" },
+    { id: 2, title: "Conta de Luz", type: false, amount: 100, bank: "NUBank", category: "Trabalho", createdAt: "2020-01-01" },
+    { id: 3, title: "Conta de Luz", type: true, amount: 100, bank: "Santander", category: "Diversão", createdAt: "2020-01-01" },
+    { id: 4, title: "Conta de Luz", type: false, amount: 100, bank: "Caixa", category: "Supermecado", createdAt: "2020-01-01" },
+    { id: 5, title: "Conta de Luz", type: true, amount: 100, bank: "À vista", category: "Saúde", createdAt: "2020-01-01" },
   ];
 
   let catFilt = transactions.filter(function (obj) { if (categoria != undefined) return obj.category == categoria; else return obj; });
-  let bankFilt = transactions.filter(function (obj) { if (categoria != undefined) return obj.bank == banco; else return obj; });
-  let typeFilt = transactions.filter(function (obj) { if (categoria != undefined) return obj.type == tipo; else return obj; });
+  let bankFilt = transactions.filter(function (obj) { if (banco != undefined) return obj.bank == banco; else return obj; });
+  let typeFilt = transactions.filter(function (obj) { if (tipo != undefined) return obj.type == tipo; else return obj; });
 
   // const transactionsFiltradas = [];
 
@@ -57,6 +57,41 @@ export function TransactionsTable() {
           { label: "Diversão", value: "Diversão" },
           { label: "Supermecado", value: "Supermecado" },
           { label: "Saúde", value: "Saúde" },
+        ]}
+      />
+      <RNPickerSelect
+        placeholder={{ label: "Selecione banco utilizado" }}
+        style={{
+          inputAndroid: {
+            backgroundColor: "#e7e9ee",
+            marginTop: 8,
+            marginBottom: 8,
+          },
+        }}
+        onValueChange={(value) => setBanco(value)}
+        value={banco}
+        items={[
+          { label: "À vista", value: "À vista" },
+          { label: "Itaú", value: "Itaú" },
+          { label: "NUBank", value: "NUBank" },
+          { label: "Santander", value: "Santander" },
+          { label: "Caixa", value: "Caixa" },
+        ]}
+      />
+      <RNPickerSelect
+        placeholder={{ label: "Selecione banco utilizado" }}
+        style={{
+          inputAndroid: {
+            backgroundColor: "#e7e9ee",
+            marginTop: 8,
+            marginBottom: 8,
+          },
+        }}
+        onValueChange={(value) => setTipo(value)}
+        value={tipo}
+        items={[
+          { label: "Entrada", value: true },
+          { label: "Saida", value: false },
         ]}
       />
       {transactions &&
