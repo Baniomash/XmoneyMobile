9d525210b13a629827f7cd57d5fc510cb7f32b3a
Hari Junior <antonio.hari@hotmail.com>
2022-08-13 20:29:25 -0300

    feat: add filter

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { ScrollView, Text } from "react-native";
+import React, { useState } from "react";
+import { Text } from "react-native";
 import { useTransactions } from "../../hooks/useTransactions";
+import RNPickerSelect from "react-native-picker-select";
 import {
   Banco,
   Cabeçalho,
@@ -17,27 +18,50 @@ import {
   Valor,
 } from "./styles";
 export function TransactionsTable() {
+  // const { transactions } = useTransactions()
 
-  const { transactions } = useTransactions()
+  const [categoria, setCategoria] = useState(undefined);
+  const [banco, setBanco] = useState(undefined);
+  const [tipo, setTipo] = useState(undefined);
 
-  console.log(transactions)
+  const transactions = [
+    { id: 1, title: "Conta de Luz", type: true, amount: 100, bank: "Itau", category: "Moradia", createdAt: "2020-01-01" },
+    { id: 2, title: "Conta de Luz", type: false, amount: 100, bank: "Itau", category: "Trabalho", createdAt: "2020-01-01" },
+    { id: 3, title: "Conta de Luz", type: true, amount: 100, bank: "Itau", category: "Diversão", createdAt: "2020-01-01" },
+    { id: 4, title: "Conta de Luz", type: false, amount: 100, bank: "Itau", category: "Supermecado", createdAt: "2020-01-01" },
+    { id: 5, title: "Conta de Luz", type: true, amount: 100, bank: "Itau", category: "Saúde", createdAt: "2020-01-01" },
+  ];
 
-  // const transactions = [
-  //   { id: 1, title: "Conta de Luz", type: true, amount: 100, bank: "Itau", category: "Energia", createdAt: "2020-01-01" },
-  //   { id: 2, title: "Conta de Luz", type: false, amount: 100, bank: "Itau", category: "Energia", createdAt: "2020-01-01" },
-  //   { id: 3, title: "Conta de Luz", type: true, amount: 100, bank: "Itau", category: "Energia", createdAt: "2020-01-01" },
-  //   { id: 4, title: "Conta de Luz", type: false, amount: 100, bank: "Itau", category: "Energia", createdAt: "2020-01-01" },
-  // ];
-  
+  let catFilt = transactions.filter(function (obj) { if (categoria != undefined) return obj.category == categoria; else return obj; });
+  let bankFilt = transactions.filter(function (obj) { if (categoria != undefined) return obj.bank == banco; else return obj; });
+  let typeFilt = transactions.filter(function (obj) { if (categoria != undefined) return obj.type == tipo; else return obj; });
+
+  // const transactionsFiltradas = [];
+
+  // transactionsFiltradas.push(catFilt, bankFilt, typeFilt);
+
   return (
     <Container>
       <Topo>
         <Text>Listagem</Text>
-        <Text>Quantidade: </Text>
+        <Text>Quantidade:</Text>
       </Topo>
+      <RNPickerSelect
+        placeholder={{ label: "Selecione a categoria" }}
+        style={{ inputAndroid: { backgroundColor: "#e7e9ee" } }}
+        onValueChange={(value) => setCategoria(value)}
+        value={categoria}
+        items={[
+          { label: "Moradia", value: "Moradia" },
+          { label: "Trabalho", value: "Trabalho" },
+          { label: "Diversão", value: "Diversão" },
+          { label: "Supermecado", value: "Supermecado" },
+          { label: "Saúde", value: "Saúde" },
+        ]}
+      />
       {transactions &&
         <Scroll >
-          {transactions.map(transaction => (
+          {catFilt.map(transaction => (
             <Card key={transaction.id}>
               <Conteudo>
                 <Header>
