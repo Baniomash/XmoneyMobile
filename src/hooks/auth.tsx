4861ace961dc41f4b511efdfe3f88e326b734d10
Hari Junior <antonio.hari@hotmail.com>
2022-08-19 16:09:38 -0300

    fix: create user bug

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,66 +1,69 @@
 import {
-    createContext,
-    ReactNode,
-    SetStateAction,
-    useContext,
-    useEffect,
-    useState,
-  } from "react";
-  
-  const { api } = require("../services/api");
-  
-  export interface Usuarios {
-    id: number;
-    login: string;
-    password: string;
+  createContext,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
+const { api } = require("../services/api");
+
+export interface Usuarios {
+  id: number;
+  login: string;
+  password: string;
+
+}
+
+type LoginInput = Omit<Usuarios, "id">;
+
+interface UsuariosProviderProps {
+  children: ReactNode;
+}
+
+interface UsuariosContextData {
+  usuario: Usuarios[];
+  createUser: (usuario: LoginInput) => Promise<void>;
+}
+
+const UsuariosContext = createContext<UsuariosContextData>(
+  {} as UsuariosContextData
+);
+
+export function UsuariosProvider({ children }: UsuariosProviderProps) {
+  const [usuario, setUsuario] = useState<Usuarios[]>([]);
+
+  async function getUsers() {
+    await api
+      .get("Users")
+      .then((response: { data: SetStateAction<Usuarios[]>; }) => setUsuario(response.data));
   }
-  
-  type LoginInput = Omit<Usuarios, "id">;
-  
-  interface UsuariosProviderProps {
-    children: ReactNode;
-  }
-  
-  interface UsuariosContextData {
-    usuario: Usuarios[];
-    createUser: (usuario: LoginInput) => Promise<void>;
-  }
-  
-  const UsuariosContext = createContext<UsuariosContextData>(
-    {} as UsuariosContextData
-  );
-  
-  export function UsuariosProvider({ children }: UsuariosProviderProps) {
-    const [usuario, setUsuario] = useState<Usuarios[]>([]);
-  
-    useEffect(() => {
-      api
-        .get("Users")
-        .then((response: { data: SetStateAction<Usuarios[]>; }) => setUsuario(response.data));
-    }, []);
-  
-    async function createUser(usuarioInput: LoginInput) {
-      await api.post("/Users", 
-        usuarioInput
-      );
-    }
-  
-    return (
-      <UsuariosContext.Provider
-        value={{
-            usuario,
-            createUser,
-        }}
-      >
-        {children}
-      </UsuariosContext.Provider>
+
+  useEffect(() => {
+    getUsers()
+  }, []);
+
+  async function createUser(usuarioInput: LoginInput) {
+    await api.post("/Users",
+      usuarioInput
     );
+    getUsers()
   }
-  
-  export function useUsuarios() {
-    const context = useContext(UsuariosContext);
-  
-    return context;
-  }
-  
+
+  return (
+    <UsuariosContext.Provider
+      value={{
+        usuario,
+        createUser,
+      }}
+    >
+      {children}
+    </UsuariosContext.Provider>
+  );
+}
+
+export function useUsuarios() {
+  const context = useContext(UsuariosContext);
+  return context;
+}
